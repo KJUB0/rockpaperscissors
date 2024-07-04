700a091af0a36149b40ade1dec7970e42d545547
KJUB0 <jakubferencik3@gmail.com>
2024-07-04 22:23:09 +0200

    zakladny html file + getcomputerchoice

---
+++ script.js
@@ -0,0 +1,13 @@
+function GetComputerChoice() {
+    const choice = Math.floor(Math.random() * 3) + 1;
+        
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(GetComputerChoice());
